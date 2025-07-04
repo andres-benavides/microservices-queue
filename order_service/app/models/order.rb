1576d41a19403d6945a25d3e4d83b7ade867a7b3
Andres Benavides <andres.benavides@rankmi.com>
2025-07-04 08:30:21 -0500

    feat: default status order

--- order_service/app/models/order.rb
+++ order_service/app/models/order.rb
@@ -1,2 +1,13 @@
 class Order < ApplicationRecord
+  enum status: { created: 0, progress: 1, finish: 2 }
+
+  validates :customer_id, :product_name, :quantity, :price, :status, presence: true
+
+  after_initialize :set_default_status, if: :new_record?
+
+  private
+
+  def set_default_status
+    self.status ||= :created
+  end
 end
